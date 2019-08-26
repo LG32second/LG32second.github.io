1aef905785a1b36b2c2e78553fc851c979800313
LG32 <1073684452@qq.com>
2019-08-26 15:33:24 +0800

    该版本为开始修改的第一版

--- armDemo.js
+++ armDemo.js
@@ -1,18 +1,21 @@
-
+//https://lg32second.github.io/armDemo.js
 
 (function(ext) {
 
-    var currentBaud = 115200;
-    var currentStatus = 1;
+    let currentBaud = 115200;
+    let currentStatus = 1;
+    let connectStatus = false;
+    let tempCom = "请选择端口";
+
 
     // 当插件退出时要做的事情
     ext._shutdown = function() {};
 
     // 状态描述，用于提示插件的错误信息，比如不支持浏览器及版本等
     ext._getStatus = function() {
-        var tmpResult = {status: 1, msg: '等待连接'};
+        let tmpResult = {status: 1, msg: '等待连接'};
 
-        if(currentStatus == 2)
+        if(currentStatus === 2)
         {
             tmpResult.status = 2;
             tmpResult.msg = '连接成功';
@@ -21,20 +24,15 @@
         return tmpResult;
     };
 
-    ext.put_up = function(){
+    /**
+     * 刷新串口列表
+     */
+    ext.fresh_serialPort = function(){
         $.ajax({
-            url:'http://localhost:8800/p',
-            data:{
-                dt:1
-            }
-        });
-    };
-
-    ext.put_down = function(){
-        $.ajax({
-            url:'http://localhost:8800/p',
-            data:{
-                dt:2
+            url: 'http://localhost:8800/fresh_serialPort',
+            data: {},
+            success: function (msg) {
+                console.log("fresh_serialPort" + msg)
             }
         });
     };
@@ -75,18 +73,37 @@
         });
     };
 
+    /**
+     * 发送新数据
+     * @param msg
+     */
     ext.send_message = function(msg){
         $.ajax({
             url:'http://localhost:8800/msg',
             data:{
                 dt:msg
+            },
+            success: function (msg) {
+                console.log("send_message success" + msg)
+            },
+            error: function (msg) {
+                console.log("send_message error: " + msg)
             }
         });
     };
 
+    /**
+     * 查看连接状态
+     * @return connectStatus为Boolean，连接成功为true，失败为false
+     */
     ext.check_connection = function(){
+        return connectStatus;
     };
 
+    /**
+     * 接受新数据
+     * @param callback
+     */
     ext.get_last_message = function(callback){
         $.ajax({
             url:'http://localhost:8800/m',
@@ -104,16 +121,20 @@
         });
     };
 
+    /**
+     * 设置波特率
+     * @param baudRate
+     * @returns {*}
+     */
     ext.set_baud_rate = function(baudRate){
         return baudRate;
     };
 
     // 模块描述
-    var descriptor = {
+    let descriptor = {
         blocks: [
             // 模块类型, 模块名称, 对应方法名称，参数依次对应的默认值
-            [' ', '抬 起 ', 'put_up'],
-            [' ', '放 下 ', 'put_down'],
+            [' ', '刷新串口', 'fresh_serialPort'],
             [' ', '向 前 ', 'move_forward'],
             [' ', '向 后 ', 'move_backword'],
             [' ', '左 转 ', 'hand_catch'],
@@ -121,13 +142,15 @@
             [' ', '发送数据 %s ', 'send_message','1'],
             ['b', '连接成功', 'check_connection'],
             ['r', '最新接受数据', 'get_last_message'],
-            ['r', "波特率: %m.baudRates", 'set_baud_rate', currentBaud]
+            ['r', "波特率: %m.baudRates", 'set_baud_rate', currentBaud],
+            ['r', "端口：%m.coms", 'set_com', tempCom]
         ],
         menus:{
             my_first_menu:['one','two','three'],
-            baudRates: [2400, 9600, 19200, 38400, 57600, 115200]
+            baudRates: [2400, 9600, 19200, 38400, 57600, 115200],
+            coms: []
         },
-        url:'https://github.com/zacSuo/ScratchSerial',
+        url:'https://github.com/LG32second/LG32second.github.io',
         displayName: 'scratch串口通讯测试'
     };
 
--- server/server.js
+++ server/server.js
@@ -1,67 +1,107 @@
-var SerialPort = require('SerialPort');
-var url = require('url')
+const SerialPort = require('SerialPort');
+const url = require('url');
+const server = require("http");
 
-var serialPort = new SerialPort("COM1", {
-  baudRate: 115200
-});
+let serialPort = null;
+let com = [];
+let baudRate = 115200;
 
-function write2Serial(data){
+/**
+ * nodeJs服务初始化
+ */
+server.createServer(function (req, res) {
+    let buffSend = decodeMsg(req.url);
+    write2Serial(buffSend);
+    res.writeHead(200, {"Content-type": "text/plain"});
+    res.write("hello");
+    res.end();
+}).listen(8800);
+
+function freshPort() {
+    console.log('进入刷新串口函数');
+    const arr = [];
+    SerialPort.list((err, ports) => {
+        for (let item of ports) {
+            const temp = {
+                value: ''
+            };
+            temp.value = item.comName;
+            console.log(item.comName);
+            arr.push(temp);
+        }
+        com = arr;
+    });
+    return com;
+}
+
+/**
+ * 连接串口
+ */
+function serialComm() {
+    console.log('串口正在打开');
+    serialPort = new SerialPort(com, {
+        baudRate: baudRate
+    });
+}
+
+/**
+ * 关闭串口
+ */
+function serialClose() {
+    console.log('正在关闭串口');
+    serialPort.close()
+}
+
+function write2Serial(data) {
     serialPort.write(data);
 }
 
-function decodeMsg(reqUrl)
-{
+function decodeMsg(reqUrl) {
     // var buff = [0xFE,0x05,0x93,0xCF,0x7D,0x5A,0x00,0xFF];
-    var buff = [0xFF, 0x00, 0xFE]
-    var urlInfo = url.parse(reqUrl,true);
-    var urlPath = urlInfo.pathname;
-    var intValue = urlInfo.query.dt;
-    switch (urlPath)
-    {
-        case '/p':
-        if(intValue == 1){
-            buff[1] = 0xA2
-        }else{
-            buff[1] = 0xA3
-        }
-        break;
+    let buff = [0xFF, 0x00, 0xFE];
+    let urlInfo = url.parse(reqUrl, true);
+    let urlPath = urlInfo.pathname;
+    let intValue = urlInfo.query.dt;
+    switch (urlPath) {
+        case '/fresh_serialPort':
+            // if (intValue === 1) {
+                return freshPort();
+            // } else {
+            //     buff[1] = 0xA3
+            // }
+            break;
+
         case '/m':
-        if(intValue == 1){
-            buff[1] = 0xA4
-        }else{
-            buff[1] = 0xA5
-        }
-        break;
+            if (intValue === 1) {
+                buff[1] = 0xA4
+            } else {
+                buff[1] = 0xA5
+            }
+            break;
+
         case '/h':
-        if(intValue == 1){
-            buff[1] = 0xA6
-        }else{
-            buff[1] = 0xA7
-        }
-        break;
+            if (intValue === 1) {
+                buff[1] = 0xA6
+            } else {
+                buff[1] = 0xA7
+            }
+            break;
+
         case '/msg':
-        var msgLen = intValue.length;
+            let msgLen = intValue.length;
 
-        buff = [0xFE,0x04 + msgLen,0x93,0xCF,0x7D,0x5A];
+            buff = [0xFE, 0x04 + msgLen, 0x93, 0xCF, 0x7D, 0x5A];
 
-        for (var i = 0; i < msgLen; i++) {
-            var s = intValue.substr(i, 1);
-            var v = parseInt(s, 16);
-            buff.push(v);
-        }
-        buff.push(0xFF);
-        break;
-        default:break;
+            for (let i = 0; i < msgLen; i++) {
+                let s = intValue.substr(i, 1);
+                let v = parseInt(s, 16);
+                buff.push(v);
+            }
+            buff.push(0xFF);
+            break;
+
+        default:
+            break;
     }
     return buff;
 }
-
-
-var server = require("http")
-server.createServer(function(req,res){
-    var buffSend = decodeMsg(req.url)
-    write2Serial(buffSend)
-    res.writeHead(200,{"Content-type":"text/plain"});
-    res.write("hello");
-    res.end();
-}).listen(8800)
